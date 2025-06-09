6ec5a6ccd62bcd33cc095fdaf1139a9993e21bff
Ak_D7 <akshitadeka7@gmail.com>
2025-06-09 21:13:01 +0530

    handling combo symbols

--- app/main.ts
+++ app/main.ts
@@ -2,15 +2,15 @@ import fs from "fs";
 const args: string[] = process.argv.slice(2); // Skip the first two arguments (node path and script path)
 
 if (args.length < 2) {
-  console.error("Usage: ./your_program.sh tokenize <filename>");
-  process.exit(1);
+	console.error("Usage: ./your_program.sh tokenize <filename>");
+	process.exit(1);
 }
 
 const command: string = args[0];
 
 if (command !== "tokenize") {
-  console.error(`Usage: Unknown command: ${command}`);
-  process.exit(1);
+	console.error(`Usage: Unknown command: ${command}`);
+	process.exit(1);
 }
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -18,8 +18,16 @@ console.error("Logs from your program will appear here!");
 
 let tokens: [string, string, string][] = [];
 let line: number = 1,
-  hasError: boolean = false;
-const identify = (character: string): string | null => {
+  hasError: boolean = false, index ={ value: 0 };
+
+const checkNextChar = (nextChar:string, index:{value:number}):boolean=>{
+	if(index.value==fileContent.length-1 || fileContent[index.value+1]!=="=")
+		return false;
+	index.value++;
+	return true;
+}
+
+const identify = (character: string, index:{value:number}): string | null => {
   switch (character) {
 	case "(":
 	  return "LEFT_PAREN";
@@ -51,6 +59,15 @@ const identify = (character: string): string | null => {
 	case "-":
 	  return "MINUS";
 	  break;
+	case "=":
+		return checkNextChar("=", index)?"EQUAL_EQUAL":"EQUAL";
+		break;
+	case "<":
+		return checkNextChar("=", index)?"LESS_EQUAL":"EQUAL";
+		break;
+	case ">":
+		return checkNextChar("=", index)?"GREATER_EQUAL":"EQUAL";
+		break;
 	default:
 	  console.error(`[line ${line}] Error: Unexpected character: ${character}`);
 	  hasError = true;
@@ -64,20 +81,20 @@ const filename: string = args[1];
 
 const fileContent: string = fs.readFileSync(filename, "utf8");
 
-for (let ch: number = 0; ch < fileContent.length; ch++) {
-  const lexeme: string = fileContent[ch];
-  const token_type: string | null = identify(lexeme);
-  const literal: string = "null";
-  if (token_type !== null) {
+for (; index.value < fileContent.length; index.value++) {
+	const lexeme: string = fileContent[index.value];
+	const token_type: string | null = identify(lexeme, index);
+	const literal: string = "null";
+	if (token_type !== null) {
 	tokens.push([token_type, lexeme, literal]);
-  }
+	}
 }
 
 tokens.push(["EOF", "", "null"]);
 
 for (let token: number = 0; token < tokens.length; token++) {
-  const value: [string, string, string] = tokens[token];
-  console.log(`${value[0]} ${value[1]} ${value[2]}`);
+	const value: [string, string, string] = tokens[token];
+	console.log(`${value[0]} ${value[1]} ${value[2]}`);
 }
 
 if(hasError) {
