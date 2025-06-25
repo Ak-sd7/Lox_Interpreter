068ba3e9e732817b360ac976da0aba4a5aa0ef7c
Ak_D7 <akshitadeka7@gmail.com>
2025-06-25 23:15:55 +0530

    AST

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 import { Tokenizer } from "./tokenize";
+import { Parser } from "./parse";
+import { Ast } from "./ast";
 const args: string[] = process.argv.slice(2); // Skip the first two arguments (node path and script path)
 
 if (args.length < 2) {
@@ -20,15 +22,30 @@ console.error("Logs from your program will appear here!");
 const filename: string = args[1];
 const fileContent: string = fs.readFileSync(filename, "utf8");
 
-
 const tokenizer = new Tokenizer(fileContent);
 const tokens = tokenizer.tokenize();
 
-for (let token: number = 0; token < tokens.length; token++) {
-  const value = tokens[token];
-  console.log(`${value[0]} ${value[1]} ${value[2]}`);
+if(command==="tokenize") {
+  for (let token: number = 0; token < tokens.length; token++) {
+    const value = tokens[token];
+    console.log(`${value[0]} ${value[1]} ${value[2]}`);
+  }
+
+  if (tokenizer.hasErrors()) {
+    process.exit(65);
+  }
+}else if(command==="parse") {
+  if (tokenizer.hasErrors()) {
+    process.exit(65);
+  }
+
+  const parser = new Parser(tokens);
+  const expression = parser.parse();
+
+  if(expression===null)
+    process.exit(65);
+
+  const astStruct = new Ast();
+  console.log(astStruct);
 }
 
-if (tokenizer.hasErrors()) {
-  process.exit(65);
-}
