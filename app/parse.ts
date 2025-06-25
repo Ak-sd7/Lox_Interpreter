d7458c6a0d326c9f12bfe0aa45029c60f56b80c2
Ak_D7 <akshitadeka7@gmail.com>
2025-06-25 22:32:31 +0530

    Parser clas conti

--- app/parse.ts
+++ app/parse.ts
@@ -4,7 +4,8 @@ import { Binary, Unary, Grouping, Exp, Literal} from "./visitor";
 
 export class Parser {
 	private tokens: Token[];
-	private current: Number = 0;
+	private current: number = 0;
+
 	constructor(tokens:Token[]) {
 		this.tokens = tokens;
 	}
@@ -22,30 +23,96 @@ export class Parser {
 	}
 
 	private equality(): Exp {
-		let expr = this.comparision();
+		let expr = this.comparison();
 
 		while(this.match("BANG_EQUAL", "EQUAL_EQUAL")) {
-			const operator = 
+			const operator = this.previous();
+			const right = this.comparison();
+			expr = new Binary(expr, operator, right);
 		}
+		return expr;
 	}
 
-	private comparision(): Exp {
+	private comparison(): Exp {
 		let expr = this.term();
+		while(this.match("LESS", "GREATER", "GREATER_EQUAL", "LESS_EQUAL")) {
+			const operator = this.previous();
+			const right = this.term();
+			expr = new Binary(expr, operator, right);
+		}
+		return expr;
 	}
 
 	private term(): Exp {
 		let expr = this.factor();
+		while(this.match("MINUS", "PLUS")) {
+			const operator = this.previous();
+			const right = this.factor();
+			expr = new Binary(expr, operator, right);
+		}
+		return expr;
 	}
 
 	private factor(): Exp {
 		let expr = this.unary();
+		while(this.match("SLASH", "STAR")) {
+			const operator = this.previous();
+			const right = this.unary();
+			expr = new Binary(expr, operator, right);
+		}
+		return expr;
 	}
 
 	private unary(): Exp {
-		let expr = this.primary();
+		if(this.match("BANG", "MINUS")) {
+			const operator = this.previous();
+			const right = this.unary();
+			return new Unary(operator, right);
+		}
+		return this.primary();
 	}
 
 	private primary(): Exp {
+		if(this.match("FALSE"))
+			return new Literal(false);
+
+		if(this.match("FALSE"))
+			return new Literal(true);
+
 		
 	}
+
+	private match(...types: string[]): boolean {
+		for(const type of types) {
+			if(this.check(type)) 
+				this.advance();
+				return true;
+		}
+		return false;
+	}
+
+	private check(type: string): boolean {
+		if(this.isEnd())
+			return false;
+		return type === this.peek()[0];
+	}
+
+	private advance(): Token {
+		if(!this.isEnd())
+			this.current++;
+		return this.previous();
+	}
+
+	private previous(): Token {
+		return this.tokens[this.current-1];
+	}
+
+	private isEnd(): boolean {
+		return this.peek()[0] === "EOF";
+	}
+
+	private peek(): Token {
+		return this.tokens[this.current];
+	}
+
 }
